a8fc75a26ca6db65ae41b2b7bba6b308773309aa
xxinxinfeng <l>
2019-10-16 09:28:02 +0800

    1016

--- src/common/http/config.js
+++ src/common/http/config.js
@@ -5,11 +5,7 @@ export default {
 	headers: {
 		// 'Content-Type': 'application/x-www-form-urlencoded',
 		'Content-Type': 'application/json;charset=UTF-8',
-
-		// 'token': this.$cookies.get("token")
 		'token': window.localStorage.getItem("token"),
-		// 'token':""
-		// 'token': 'sinI7ifBos20190926110825273oft3kACL'
 	},
 	data: {},
 	timeout: 5000,
